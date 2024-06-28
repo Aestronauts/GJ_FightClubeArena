63826ab90a6fe6b6c373cb4b654829a7250bea00
Trisyd <gabrielpriem@gmail.com>
2024-06-27 17:54:31 -0700

    Created Testing Scene for Training Mode
    Includes a prefab of the environment blockout and a singleton pattern on a new Game Manager.

---
+++ Aestro_FightClubArena/Assets/Scripts/Players/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _Instance;
+    public  static GameManager Instance
+    {
+        get
+        {
+            if (!_Instance)
+            {
+                _Instance = new GameObject().AddComponent<GameManager>();
+                _Instance.name = _Instance.GetType().ToString();
+                DontDestroyOnLoad(_Instance.gameObject);
+            }
+            return _Instance;
+        }
+    }
+
+    public GameObject Environment1;
+    public GameObject Environment2;
+    public GameObject FireMage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
