72ccc9bbd840a957848cda4ef6096c38f3be3907
Millie Harris <msharris17@gmail.com>
2024-08-02 12:48:13 -0400

    damage collision fixes
    * updating damage collisions so abilities only hit players
    * fixing ui updating for damage over time conditions

--- Aestro_FightClubArena/Assets/Scripts/Spells/AbilityBehavior.cs
+++ Aestro_FightClubArena/Assets/Scripts/Spells/AbilityBehavior.cs
@@ -19,6 +19,10 @@ public class AbilityBehavior : MonoBehaviour
 
     private GameObject instantiatedObjectDamageCanvas = null;
     private GameObject instantiatedObjectDamageOvertimeCanvas = null;
+    
+    private bool dpsTickWaiting;
+    
+    private Coroutine triggerStayCoroutine;
 
     // // Start is called before the first frame update
     void Awake()
@@ -38,24 +42,37 @@ public class AbilityBehavior : MonoBehaviour
     
             if (distanceTraveled >= abilityParameters.range || transform.position == endLocation)
             {
-                instantiatedObjectDamageCanvas = Instantiate(DamageUIPrefab, transform.position, Quaternion.identity, AbilityManager.instance.ProjectilesHolder);
-                instantiatedObjectDamageCanvas.GetComponent<DamageUICanvas>().abilityDamage = AbilityManager.instance.abilitiesList[abilityID].damage;
                 gameObject.SetActive(false);
             }
         }
-
-        if (instantiatedObjectDamageOvertimeCanvas == null)
+    }
+    
+    //this collision check is for environment objects mostly, if it hits, disable the projectile
+    //if its not a projectile i.e non-0 duration, it stays active until the duration is up
+    //(handled in the AbilitiesHelper script)
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("InvisWall"))
         {
-            //spawn damage number for non distance traveling attacks
-            if (AbilityManager.instance.abilitiesList[abilityID].damageOvertime != 0)
+            // Disable the bullet when it hits another collider
+            if (AbilityManager.instance.abilitiesList[abilityID].duration == 0)
             {
-                instantiatedObjectDamageOvertimeCanvas = Instantiate(DamageUIPrefab, transform.position, Quaternion.identity, AbilityManager.instance.ProjectilesHolder);
-                instantiatedObjectDamageOvertimeCanvas.GetComponent<DamageUICanvas>().abilityDamage =
-                    AbilityManager.instance.abilitiesList[abilityID].damageOvertime;
+                gameObject.SetActive(false);
             }
         }
+    }
+    
+    //check if the ability cast has collided with a player, if it has
+    //spawn the damage ui canvas relevant to the ability that this is
+    //we have a check distinction between duration lengths because if its a projectile,
+    //we disable it on collision, if its a long range cast, it stays active until the duration is up
+    void OnTriggerEnter(Collider other)
+    {
+        // This method is called when another collider enters the trigger collider
+        Debug.Log("Entered trigger with: " + other.gameObject.name);
 
-        if (instantiatedObjectDamageCanvas == null)
+        // You can check for specific collision types, tags, etc.
+        if (other.gameObject.CompareTag("Player"))
         {
             if (AbilityManager.instance.abilitiesList[abilityID].duration != 0)
             {
@@ -64,17 +81,77 @@ public class AbilityBehavior : MonoBehaviour
                 instantiatedObjectDamageCanvas.GetComponent<DamageUICanvas>().abilityDamage =
                     AbilityManager.instance.abilitiesList[abilityID].damage;
             }
+
+            if (AbilityManager.instance.abilitiesList[abilityID].duration == 0)
+            {
+                instantiatedObjectDamageCanvas = Instantiate(DamageUIPrefab, transform.position, Quaternion.identity, AbilityManager.instance.ProjectilesHolder);
+                instantiatedObjectDamageCanvas.GetComponent<DamageUICanvas>().abilityDamage = AbilityManager.instance.abilitiesList[abilityID].damage;
+                gameObject.SetActive(false);
+            }
+            
         }
     }
     
-    void OnCollisionEnter(Collision collision)
+    //check if the player is standing in a damage collision area, if it is
+    //and the ability cast has a damage over time rate, do it
+    void OnTriggerStay(Collider other)
     {
-        if (!collision.gameObject.CompareTag("InvisWall"))
+        // This method is called as long as another collider stays within the trigger collider
+        Debug.Log("Staying in trigger with: " + other.gameObject.name);
+        if (other.gameObject.CompareTag("Player"))
         {
-            instantiatedObjectDamageCanvas = Instantiate(DamageUIPrefab, transform.position, Quaternion.identity, AbilityManager.instance.ProjectilesHolder);
-            instantiatedObjectDamageCanvas.GetComponent<DamageUICanvas>().abilityDamage = AbilityManager.instance.abilitiesList[abilityID].damage;
-            // Disable the bullet when it hits another collider
-            gameObject.SetActive(false);
+            if (!dpsTickWaiting)
+            {
+                if (AbilityManager.instance.abilitiesList[abilityID].DPS_rate != 0)
+                {
+                    triggerStayCoroutine = StartCoroutine(TriggerStayDPS());
+                }
+            }
+        }
+    }
+
+    //if we leave the damage overtime collision area, set dps tick waiting to false so
+    //if we re-enter it, we can take damage overtime again
+    void OnTriggerExit(Collider other)
+    {
+        StopAllCoroutines();
+        dpsTickWaiting = false;
+    }
+    
+    //make sure we set the dps tick waiting to false when we enable the
+    //object so it can spawn damage over time when its turned on
+    void OnEnable()
+    {
+        dpsTickWaiting = false;
+    }
+    
+    //make sure we set the dps tick waiting to false when we disable the
+    //object so it can spawn damage over time when its turned on again
+    void OnDisable()
+    {
+        if (triggerStayCoroutine != null)
+        {
+            StopCoroutine(triggerStayCoroutine);
+        }
+        dpsTickWaiting = false;
+    }
+
+    //call this coroutine to repeatedly spawn damage ui canvases for damage overtime. only do it
+    //if there is a damage to be applied i.e not 0
+    private IEnumerator TriggerStayDPS()
+    {
+        dpsTickWaiting = true;
+        while (true)
+        {
+            //spawn damage number for non distance traveling attacks
+            if (AbilityManager.instance.abilitiesList[abilityID].damageOvertime != 0)
+            {
+                instantiatedObjectDamageOvertimeCanvas = Instantiate(DamageUIPrefab, transform.position, Quaternion.identity, AbilityManager.instance.ProjectilesHolder);
+                instantiatedObjectDamageOvertimeCanvas.GetComponent<DamageUICanvas>().abilityDamage =
+                    AbilityManager.instance.abilitiesList[abilityID].damageOvertime;
+            }
+
+            yield return new WaitForSeconds(AbilityManager.instance.abilitiesList[abilityID].DPS_rate);
         }
     }
 }
